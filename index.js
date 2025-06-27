9a99ca44af4a073f6de33ebfe636eb6ee3b1e408
aaaqil <aaaqil.west@student.moringaschool.com>
2025-06-27 12:00:24 +0300

    Complete JavaScript functions lab - implement saturdayFun, mondayWork, and wrapAdjective

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+// saturdayFun - function declaration with default parameter
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+// mondayWork - function expression with default parameter
+const mondayWork = function(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+};
+
+// wrapAdjective - closure function that returns a function
+function wrapAdjective(flair = "*") {
+  return function(adjective = "special") {
+    return `You are ${flair}${adjective}${flair}!`;
+  };
+}
